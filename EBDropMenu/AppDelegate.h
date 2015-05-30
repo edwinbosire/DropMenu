2e20a7f877cfefb5afbb6710d8b1440218ba5a7a
Edwin Bosire <dev@edwinb.co.uk>
2015-05-30 21:56:05 +0100

    Initial Commit

---
+++ EBDropMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EBDropMenu
+//
+//  Created by edwin bosire on 30/05/2015.
+//  Copyright (c) 2015 Edwin Bosire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EBDropMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EBDropMenu
+//
+//  Created by edwin bosire on 30/05/2015.
+//  Copyright (c) 2015 Edwin Bosire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
